5f80df43b140dbc68187fbb3a0bd3accdf5da2ae
Sam Nazarko <email@samnazarko.co.uk>
2018-03-20 16:48:25 +0000

    Initial theme for OSMC
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# name: osmc
+# about: Osmc modifications for Discourse
+# version: 0.2
+# authors: Simone Uggeri (thehacker66)
+
+register_asset "javascripts/discourse/templates/preferences/account.hbs"
+register_asset "stylesheets/common/base/header.scss"
+
+PLUGIN_NAME ||= "osmc".freeze
+
+DATA_PREFIX ||= "osmc-".freeze
