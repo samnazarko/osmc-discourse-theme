24ad0d384b201588c0967600c56c83933f55a68a
TheHacker66 <thehacker66@gmail.com>
2020-06-29 14:10:40 +0200

    Added modified groups controller without limit

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: osmc
 # about: Osmc modifications for Discourse
-# version: 0.2
+# version: 0.3
 # authors: Simone Uggeri (thehacker66)
 
 register_asset "javascripts/discourse/templates/preferences/account.hbs"
@@ -10,6 +10,8 @@ register_asset "stylesheets/mobile/header.scss"
 register_asset "stylesheets/common.scss"
 register_asset "stylesheets/desktop.scss"
 register_asset "stylesheets/mobile.scss"
+
+load File.expand_path('../app/controllers/groups_controller.rb', __FILE__)
 
 PLUGIN_NAME ||= "osmc".freeze
 
